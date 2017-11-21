91ce5c216e34a9740884dfce81e0dce9aca466a7
Hirenkumar Patel <hirenkumarpatel@Hirenkumars-MacBook-Pro.local>
2017-11-21 21:23:02 +1100

    file updated for daily download

--- findMyFile.py
+++ findMyFile.py
@@ -10,10 +10,21 @@ def find(name, path):
     for f in glob.iglob(file_dir_extension):  # generator, search immediate subdirectories
         print(f)
 
-
+def dailyDownloadFile():
+    import time
+    import datetime
+    mydate = datetime.datetime.now()
+    file_extn = "CFS_ANZ_Daily_Download_"
+    currentMonth = mydate.strftime("%b")
+    print(mydate.strftime("%b"))
+    todayDate = time.strftime(currentMonth+"_%dth_%Y.xlsx")
+    print(file_extn+todayDate)
+    return  todayDate
 
 while True:
     givenName = input('Give file name ? >')
+    if givenName == "":
+        givenName = dailyDownloadFile()
 
     if givenName == 'done':
         break
@@ -21,6 +32,13 @@ while True:
         print('type in Give File Nmae *.extention or filr name')
         print('type in Give File Path /dir1/secondary diectory/')
         print("type 'done' to exit programe ")
+    if givenName == "sifile":
+         pathName = input('give path to get Daily down load file ? >')
+
+         pathNameExten = os.path.join(pathName, '*/')
+         #dailyDownloadFile()
+         continue
+
     else:
         pathName = input('Give file path >')
         pathNameExten = os.path.join(pathName, '*/')
